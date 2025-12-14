af28c28ac1c9b08e66f8cfb709481060750db987
jmvillanueva-dev <jhonny.villanueva@epn.edu.ec>
2025-12-14 13:59:12 -0500

    Add missing import statement for Equatable in usecase.dart

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import '../error/failures.dart';
+import 'package:equatable/equatable.dart';
 
 abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
